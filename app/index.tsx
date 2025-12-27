5da7a17177873ef2127338a4cdca1efc168439c2
sim <simonas.b@hotmail.com>
2025-12-27 22:49:15 +0000

    Update react-native packages

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500 border-2 p-4 rounded-lg">
+      <Text className="text-xl font-bold text-blue-500 border-2 p-4 rounded-lg bg-slate-400 border-red-700">
         Welcome to Nativewindzzz!
       </Text>
     </View>
