48830791890682b260748d7184046c066217be27
sim <simonas.b@hotmail.com>
2025-12-30 21:57:51 +0000

    Add fonts, tailwind themes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,22 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import './globals.css'
+import { useFonts } from "expo-font"
+import { useEffect } from "react";
 
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  })
+
+  useEffect(() => {
+    if(error) throw error
+    if(fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded, error])
+
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
 import { Text, View } from "react-native";
  
+
+const API_URL = "http://10.0.2.2:6767/api/v1/listings";
+
 export default function Index() {
+
+  const onTouch = async () => {
+    console.log("im touched at ", new Date().toISOString())
+
+    try {
+      console.log("starting api call to ", API_URL)
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log("finished api call to ", API_URL)
+      console.log("Listings:", data);
+    } catch (error) {
+      console.log("hi")
+      console.error("API error:", error);
+    }
+    console.log("bye")
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500 border-2 p-4 rounded-lg bg-slate-400 border-red-700">
+      <Text onPress={onTouch} className="font-quicksand-bold text-3xl text-center text-primary  border-4 border-primary p-4 rounded-lg ">
         Welcome to Nativewindzzz!
       </Text>
     </View>
